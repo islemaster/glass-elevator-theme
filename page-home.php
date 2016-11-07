f78d41492d52487db01e0ef64f17d5efcf65aae4
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-11-06 21:49:11 -0800

    Initial commit

---
+++ page-home.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<head>
+  <title>Glass Elevator Arts Consulting, LLC</title>
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo theme_url('/css/glass-elevator-arts.css'); ?>"/>
+</head>
+<body>
+  <header>
+    <h1>Glass Elevator</h1>
+    <h2>Arts Consulting</h2>
+  </header>
+  <nav>
+    <a href="/about">
+      <img
+			  src="<?php echo theme_url('img/ReginaInMorningLight_square.png'); ?>"
+				alt="About"
+				title="About Glass Elevator Arts"
+			/>
+      <span>About</span>
+    </a>
+    <a href="/artists">
+      <img
+			  src="<?php echo theme_url('img/diane_square.jpg'); ?>"
+				alt="Artists"
+				title="List of represented artists"
+			/>
+      <span>Artists</span>
+    </a>
+    <a href="/services">
+      <img
+			  src="<?php echo theme_url('img/cgag_square.jpg'); ?>"
+				alt="Services"
+				title="What we can do for you"
+			/>
+      <span>Services</span>
+    </a>
+    <a href="/gallery">
+      <img
+			  src="<?php echo theme_url('img/goofballs_square.jpeg'); ?>"
+				alt="Gallery"
+				title="See our work"
+			/>
+      <span>Gallery</span>
+    </a>
+  </nav>
+</body>
